fe437fda1489cdf19a35b77707fe938b72c24532
patrick0314 <patrick0011223344@gmail.com>
2022-04-30 23:11:40 +0800

    2022.04.30 23.11

--- main.py
+++ main.py
@@ -1,38 +1,92 @@
 from Arithmetic import arithmetic, inv_arithmetic
+from AdaptiveArithmetic import adaptivearithmetic, inv_adaptivearithmetic
 
 import time
+import sys
 
 if __name__ == '__main__':
-    print('\n= = = = = = = = = = = = = = = = = = =')
-    print('default data set is [\'a\', \'b\']')
-    print('default probability is [0.5, 0.5]')
-    print('= = = = = = = = = = = = = = = = = = =')
-
-    # Customization
-    data_set = ['a', 'b']
-    probability = [0.5, 0.5]
-
     while True:
-        # User Input
-        text = input('Enter your text wanted to encoding like \'aba\': ')
+        mode = input('Enter Arithmetic Coding for \'0\' and Adaptive Arithmetic Coding for \'1\': ')
+        if mode == '0':
+            print('\n===== Arithmetic Coding =====')
+            # Default or Customization
+            data_set = ['a', 'b']
+            probability = [0.8, 0.2]
+            # Fool Proof
+            if len(data_set) != len(probability) or sum(probability)-1>0.00000000001:
+                print('SOMETHING WRONG ABOUT DATA SET OR PROBABILITY')
+                sys.exit()
+            print('Data Set = {}'.format(data_set))
+            print('Probability of Data = {}\n'.format(probability))
 
-        if text == 'quit':
-            print('\n=== See ya !!! ===')
-            break
+            # Arithmetic Coding
+            while True:
+                # User Input
+                text = input('Enter your text message wanted to encoding: ')
 
-        if data_set != ['a', 'b'] or probability != [0.5, 0.5]:
-            print('\n= = = = = = = = = = = = = = = = = = =')
-            print('default data set is {}'.format(data_set))
-            print('default probability is {}'.format(probability))
-            print('= = = = = = = = = = = = = = = = = = =')
+                # Quit Commend
+                if text == 'quit':
+                    print('\n===== See ya !!! ======')
+                    sys.exit()
 
-        # Computation of Arithmetic Coding
-        time_start = time.time()
-        ciphertext = arithmetic(text, len(text), data_set, probability)
-        recovered_text = inv_arithmetic(ciphertext, len(text), data_set, probability)
-        time_end = time.time()
+                # Change Mode
+                if text == 'change':
+                    break
+                
+                # Fool Proof
+                break_while = False
+                for t in text:
+                    if t not in data_set:
+                        print('SOMETHING WRONG IN THE TEXT !!!')
+                        sys.exit()
+                
+                # Computation
+                time_start = time.time()
+                ciphertext = arithmetic(text, data_set, probability)
+                recovered_text = inv_arithmetic(ciphertext, len(text), data_set, probability)
+                time_end = time.time()
+                print('\nTime Cost = {}'.format(time_end-time_start))
+                print('Arithmetic coding of \'{}\' = {}'.format(text, ciphertext))
+                print('Recovered text = \'{}\''.format(recovered_text))
+                if text == recovered_text: print('Successful Coding\n')
+                else: print('Failed Coding\n')
 
-        # Output
-        print('\nAfter {} (sec), encoding and decoding are completely finished'.format(time_end-time_start))
-        print('Arithmetic coding of \'{}\' = {}'.format(text, ciphertext))
-        print('Recovered text = \'{}\'\n'.format(recovered_text))
+        elif mode == '1':
+            print('\n===== Adaptive Arithmetic Coding =====')
+            # Default or Customization
+            data_set = ['a', 'b']
+            print('Data Set = {}\n'.format(data_set))
+
+            # Adaptive Arithmetic Coding
+            while True:
+                # User Input
+                text = input('Enter your text message wanted to encoding: ')
+
+                # Quit Commend
+                if text == 'quit':
+                    print('\n===== See ya !!! ======')
+                    sys.exit()
+
+                # Change Mode
+                if text == 'change':
+                    break
+                
+                # Fool Proof
+                break_while = False
+                for t in text:
+                    if t not in data_set:
+                        print('SOMETHING WRONG IN THE TEXT !!!')
+                        sys.exit()
+                
+                # Computation
+                time_start = time.time()
+                ciphertext = adaptivearithmetic(text, data_set)
+                recovered_text = inv_adaptivearithmetic(ciphertext, len(text), data_set)
+                time_end = time.time()
+                print('\nTime Cost = {}'.format(time_end-time_start))
+                print('Arithmetic coding of \'{}\' = {}'.format(text, ciphertext))
+                print('Recovered text = \'{}\'\n'.format(recovered_text))
+                if text == recovered_text: print('Successful Coding\n')
+                else: print('Failed Coding\n')
+        else:
+            print('ONLY 0 OR 1 !!!!!!')
