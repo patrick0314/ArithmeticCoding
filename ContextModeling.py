426b375fa1f4036cec2bb2ad7adb6657ee53eed2
patrick0314 <patrick0011223344@gmail.com>
2022-05-02 00:25:06 +0800

    2022.05.02 00.24'

---
+++ ContextModeling.py
@@ -0,0 +1,9 @@
+def contextmodeling(dic, t, window):
+    return
+
+if __name__ == '__mian__':
+    text = 'aaabaa'
+    dic = {'total':0}
+    window = 3
+    for t in text:
+        contextmodeling(dic, t, window)
